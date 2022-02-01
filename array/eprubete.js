9ef9a0fbeaf3b2b1afd51959db71c694d86c41d5
Aurel1971 <augustindinu@gmail.com>
2022-02-01 20:47:13 +0200

    Curs 5

---
+++ array/eprubete.js
@@ -0,0 +1,50 @@
+
+var scores =[50, 65, 47, 81, 20, 55, 19, 91, 58, 60]
+
+document.write ('Al doilea score este:' + scores[1] + '<br>')
+document.write ('Ultimul score este:' + scores[9] + '<br>')
+
+var max = 0 
+for (let i = 0; i < scores.length; i++) {
+   if (scores[i] > max) {
+       max = scores[i]
+   }
+    
+}
+document.write('Scorul maxim este:' + max + '<br>')
+
+// ordoneaza elementele din array in ordine ascendenta
+scores.sort()
+document.write(scores + '<br>')
+
+var fruits = ['orange', 'banana', 'apple', 'wasabi' ]
+
+fruits[0] = 'kiwi'
+
+// push adauga la sfarsitul array-ului 
+// pop scoate de la sfarsit
+fruits.push('pear')
+
+// unshift adauga la inceputul array-ului
+// shift scoate de la inceput
+fruits.unshift('strawberry')
+
+fruits.sort()
+document.write(fruits + '<br>')
+// sterge doua  elemente incepand de la pozitia 1
+fruits.splice(1,2)
+document.write(fruits + '<br>')
+
+var words = ['hello', 'Visual Studio', 'Java Script', 'if', 'object']
+
+var upperWords = []
+
+for (const word of words) {
+    // word[0] este prima litera, verificam ca se afla intre A-Z
+    // o alta varianta: word[0].toUpperCase () == word[0]
+    if (word[0] >='A' && word[0] <='Z') {
+        upperWords.push(word)
+    }
+}
+upperWords.sort()
+document.write(upperWords)
--- dreptunghi/d.js
+++ dreptunghi/d.js
@@ -1,6 +1,8 @@
 var dr = document.querySelector('#dreptunghi')
+var cerc = document.querySelector('#cerc')
 
 dr.addEventListener('click', minimize)
+cerc.addEventListener('click', move)
 
 async function minimize () {
     for(let i = 10; i >= 0; i-- ){
@@ -10,4 +12,12 @@ async function minimize () {
 
     }
     
+}
+async function move () {
+    for(let i = 10; i <= 200; i++ ){
+        // asteptam 20 de ms:
+        await new Promise(r => setTimeout(r, 20))
+        cerc.style.top = cerc.style.left = i + 'px'
+    }
+    cerc.removeEventListener('click', move)
 }
